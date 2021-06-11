7f703d51ef67e494804bcbae182513ae3baaa4af
Art <ya.Artriches@ya.ru>
2021-06-11 20:53:21 +0300

    init project

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = document.querySelector('#root');
+root.innerHTML = '<h1>Hello World!</h1>';
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
