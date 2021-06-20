952c7057e8841c95c07ebd182843acede2c4ff03
Art <ya.Artriches@ya.ru>
2021-06-20 22:33:09 +0300

    add: locales

---
+++ src/locales/en.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    language: 'en',
+    languageSelect: 'language select',
+    title: 'RSS aggregator',
+    subTitle: "Start reading RSS today! It's easy, it's beautiful.",
+    posts: 'Posts',
+    feeds: 'Feeds',
+    form: {
+      placeholder: 'RSS link',
+      add: 'Add',
+      example: 'Example: https://ru.hexlet.io/lessons.rss',
+      feedback: {
+        error: 'Resource does not contain valid RSS',
+        invalid: 'The link must be a valid URL',
+        success: 'RSS loaded successfully',
+      },
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    language: 'ru',
+    languageSelect: 'Выбор языка',
+    title: 'RSS агрегатор',
+    subTitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    posts: 'Посты',
+    feeds: 'Фиды',
+    form: {
+      placeholder: 'Ссылка RSS',
+      add: 'Добавить',
+      example: 'Пример: https://ru.hexlet.io/lessons.rss',
+      feedback: {
+        error: 'Ресурс не содержит валидный RSS',
+        invalid: 'Ссылка должна быть валидным URL',
+        success: 'RSS успешно загружен',
+      },
+    },
+  },
+};
