dc2c0081db3c553b5016c138e6add9f20427a401
Art <ya.Artriches@ya.ru>
2021-06-21 17:22:30 +0300

    feat: entry point

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import i18next from 'i18next';
 import onChange from 'on-change';
-import render from './render';
+import * as yup from 'yup';
+import renderUI from './render/renderUI';
+import addHandlers from './handlers/addHandlers';
 import resources from './locales';
+import renderErrorMessage from './render/renderFeedbackMessage';
 
 const DEFAULT_LANGUAGE = 'en';
 
-export default async function app() {
+const isValidUrl = url => {
+  const schema = yup.string().required().url();
+  return schema.isValid(url);
+};
+
+export default function app() {
   const i18nInstance = i18next.createInstance();
-  await i18nInstance.init({
+  i18nInstance.init({
     lng: DEFAULT_LANGUAGE,
     fallbackLng: Object.keys(resources),
     resources,
@@ -15,18 +23,23 @@ export default async function app() {
 
   const state = {
     language: DEFAULT_LANGUAGE,
+    formRss: {
+      url: '',
+      valid: true,
+      error: false,
+      success: false,
+    },
   };
 
   const containers = {
     title: document.querySelector('#title'),
     subTitle: document.querySelector('#subTitle'),
     languageSelect: document.querySelector('#languageSelect'),
-    formRss: document.querySelector('#rss'),
+    rssForm: document.querySelector('#rss'),
     formInput: document.querySelector('#rss input'),
     formLabel: document.querySelector('#rss label'),
     submitButton: document.querySelector('#rss button'),
     exampleMessage: document.querySelector('#example'),
-    feedbackMessage: document.querySelector('#feedback'),
     feedsContainer: document.querySelector('#feeds'),
     postsContainer: document.querySelector('#posts'),
   };
@@ -36,12 +49,27 @@ export default async function app() {
       case 'language':
         i18nInstance
           .changeLanguage(value)
-          .then(() => render(containers, watchedState, i18nInstance));
+          .then(() => renderUI(containers, i18nInstance));
+        break;
+      case 'formRss.url':
+        isValidUrl(value)
+          .then(isValid => {
+            watchedState.formRss.valid = isValid;
+
+            if (!isValid) {
+              throw new Error(i18nInstance.t('form.feedback.error'));
+            }
+
+            return value;
+          })
+          .then(url => console.log(url))
+          .catch(error => renderErrorMessage(containers, error.message));
         break;
       default:
         break;
     }
   });
 
-  render(containers, watchedState, i18nInstance);
+  renderUI(containers, i18nInstance);
+  addHandlers(containers, watchedState);
 }
